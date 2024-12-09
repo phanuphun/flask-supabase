16daacd3b274b28da0661e2619959a19433d0e6a
parnuphun <81858246+parnuphun@users.noreply.github.com>
2024-12-09 21:14:20 +0700

    feat (books) : c r u d

--- app/router/books_route.py
+++ app/router/books_route.py
@@ -1,15 +1,109 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify , request
+from app.config.supabase_client import supabase
+import datetime
 
 books_route = Blueprint('main', __name__)
-@books_route.route('/')
-def hello_world():
-    return '<h1><center>Hello Flask API !!!</center></h1>'
 
+# get
 @books_route.route('/books', methods=['GET'])
 def get_books():
-    return jsonify({"message": "This is the books endpoint."})
+    try:
+        res = (supabase
+               .table('books')
+               .select('*')
+               .execute())
+        
+        return jsonify({'msg':'get books success!!' , 'data':res.data}) , 200
+        
+    except Exception as e :
+        print('Error : ',str(e))
+        return jsonify('Error : ',str(e)), 500
 
-# @books_route.route('/books', method=['POST'])
-# def method_name():
-#     try:
-#     except:
+# get book detail
+@books_route.route('/books/<string:id>', methods=['GET'])
+def get_book_detail(id):
+    try:
+        res = (supabase
+                .table('books')
+                .select('*')
+                .eq('id',str(id))
+                .execute())
+        
+        return jsonify({'msg':'get books detail success!!' , 'data':res.data}) , 200
+    except Exception as e :
+        print('Error : ',str(e))
+        return jsonify('Error : ',str(e)), 500
+
+# create
+@books_route.route('/books',methods=['POST'])
+def add_book():
+    try:
+        body = request.get_json()
+        title = body['title']
+        price = float(body['price'])
+        author = body['author']
+        category_id = body['category_id']
+        create_date = str(datetime.datetime.now())
+        
+        res = (supabase
+               .table('books')
+               .insert({
+                   'title':title,
+                   'price':price,
+                   'author':author,
+                   'category_id':category_id,
+                   'create_date':create_date
+               })
+               .execute())
+
+        return jsonify({'msg':'add books success!!' , 'data':res.data}) , 201
+        
+    except Exception as e :
+        print('Error : ',str(e))
+        return jsonify('Error : ',str(e)), 500
+ 
+# update
+@books_route.route('/books',methods=['PUT'])
+def update_book():
+    try:
+        body = request.get_json()
+        id = str(body['id'])
+        title = body['title']
+        price = float(body['price'])
+        author = body['author']
+        category_id = body['category_id']
+        create_date = str(datetime.datetime.now())
+        
+        res = (supabase
+                .table('books')
+                .update({
+                    'title':title,
+                    'price':price,
+                    'author':author,
+                    'category_id':category_id,
+                    'create_date':create_date
+                })
+                .eq('id',id)
+                .execute())
+        return jsonify({'msg':'update books success!!' , 'data':res.data}) , 200
+    
+    except Exception as e :
+        print('Error : ',str(e))
+        return jsonify('Error : ',str(e)), 500
+    
+# delete
+@books_route.route('/books',methods=['DELETE'])
+def delete_books():
+    try:
+        body = request.get_json()
+        book_id = body['id'] 
+        res = (supabase
+               .table('books')
+               .delete()
+               .eq('id',str(book_id))
+               .execute())
+        
+        return jsonify({'msg':'delete books success!!' , 'data':res.data}) , 200
+    except Exception as e :
+        print('Error : ',str(e))
+        return jsonify('Error : ',str(e)), 500
